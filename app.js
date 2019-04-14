bf4b343b932bb083abe2871c0a2154ca9581cd13
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2019-04-14 12:46:59 +0300

    examples of ejs, jade, handlebars TE

--- app.js
+++ app.js
@@ -3,29 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const http = require('http');
-
-const adminData = require('./routes/admin');
-
-const shopRoutes = require('./routes/shop');
-
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
-
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((request, response, next) => {
-    response.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
