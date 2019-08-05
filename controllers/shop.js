2556bbfdb5a74d99b6299926d9e1c1e65be18c7c
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2019-08-05 10:46:54 +0300

    * order details page

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res, next) => {
+    const productId = req.params['productId'];
+    Product.findById(productId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        })
+    })
+};
+
 exports.getIndex = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop/index', {
@@ -25,6 +36,10 @@ exports.getCart = (req, res, next) => {
       path: '/cart',
       pageTitle: 'Your Cart'
   })
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
 };
 
 exports.getOrders = (req, res, next) => {
