735216ad32d802aa4af687637490d13927f2ffa8
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2019-10-28 23:57:58 +0200

    added cart functionality

--- models/cart.js
+++ models/cart.js
@@ -42,6 +42,9 @@ module.exports = class Cart {
           const cart  = JSON.parse(fileContent);
           const updatedCart = {...cart};
           const product = updatedCart.products.find(prod => prod.id === id);
+          if (!product) {
+              return;
+          }
           const productQty = product.qty;
           updatedCart.products = updatedCart.products.filter(prod => prod.id !== product.id);
           updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
@@ -49,5 +52,16 @@ module.exports = class Cart {
               console.error(error)
           })
       });
+  }
+
+  static getCart(cb) {
+      fs.readFile(p, (err, fileContent) => {
+          const cart = JSON.parse(fileContent);
+          if(err) {
+             cb(null)
+          } else {
+              cb(cart);
+          }
+      })
   }
 };
